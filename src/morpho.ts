f8f54e340da64b91dbd0031572c6ff305d363641
Julien-devatom <julien@morpho.xyz>
2024-07-25 11:14:01 +0200

    feat(ltv): implement handlers

--- src/morpho.ts
+++ src/morpho.ts
@@ -1,111 +1,174 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts";
 import {
-  Morpho,
-  AccrueInterest,
-  Borrow,
-  CreateMarket,
-  EnableIrm,
-  EnableLltv,
-  FlashLoan,
-  IncrementNonce,
-  Liquidate,
-  Repay,
-  SetAuthorization,
-  SetFee,
-  SetFeeRecipient,
-  SetOwner,
-  Supply,
-  SupplyCollateral,
-  Withdraw,
-  WithdrawCollateral
-} from "../generated/Morpho/Morpho"
-import { ExampleEntity } from "../generated/schema"
+	AccrueInterest,
+	Borrow,
+	CreateMarket,
+	Liquidate,
+	Repay,
+	SupplyCollateral,
+	WithdrawCollateral,
+} from "../generated/Morpho/Morpho";
+import { Market, Position } from "../generated/schema";
+import { getOrCreateAsset } from "./assets";
+import { getOrCreateOracle } from "./oracles";
+import { computeLtv } from "./ltv";
 
 export function handleAccrueInterest(event: AccrueInterest): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.Morpho_id = event.params.id
-  entity.prevBorrowRate = event.params.prevBorrowRate
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.borrow(...)
-  // - contract.extSloads(...)
-  // - contract.feeRecipient(...)
-  // - contract.idToMarketParams(...)
-  // - contract.isAuthorized(...)
-  // - contract.isIrmEnabled(...)
-  // - contract.isLltvEnabled(...)
-  // - contract.liquidate(...)
-  // - contract.market(...)
-  // - contract.nonce(...)
-  // - contract.owner(...)
-  // - contract.position(...)
-  // - contract.repay(...)
-  // - contract.supply(...)
-  // - contract.withdraw(...)
+	// We leverage the fact that intestests are already accrued onchain.
+	// so we don't need to fetch the rate and accrue the interest ourselves.
+	// This is correct since the interest are accrued before every Morpho operation.
+	// If you want to have real time interest, you have to fetch the rate and accrue them on-the-fly.
+	// We don't have this problem in a subgraph
+	const market = Market.load(event.params.id);
+	if (!market) {
+		log.critical("Market {} not found", [event.params.id.toString()]);
+		return;
+	}
+	market.lastTotalBorrowAssets = market.lastTotalBorrowAssets.plus(
+		event.params.interest,
+	);
+	market.lastUpdateTimestamp = event.block.timestamp;
+	market.save();
 }
 
-export function handleBorrow(event: Borrow): void {}
+export function handleBorrow(event: Borrow): void {
+	const market = Market.load(event.params.id);
+	if (!market) {
+		log.critical("Market {} not found", [event.params.id.toString()]);
+		return;
+	}
 
-export function handleCreateMarket(event: CreateMarket): void {}
+	market.lastTotalBorrowShares = market.lastTotalBorrowShares.plus(
+		event.params.shares,
+	);
+	market.lastTotalBorrowAssets = market.lastTotalBorrowAssets.plus(
+		event.params.assets,
+	);
+	market.save();
 
-export function handleEnableIrm(event: EnableIrm): void {}
+	const position = Position.load(event.params.id.concat(event.params.onBehalf));
+	if (!position) {
+		// It cannot happen since position should have been created at the collateral deposit.
+		log.critical("Position {} not found", [
+			event.params.id.concat(event.params.onBehalf).toString(),
+		]);
+		return;
+	}
+	position.borrowShares = position.borrowShares.plus(event.params.shares);
 
-export function handleEnableLltv(event: EnableLltv): void {}
+	// computeLtv(position);
+	position.save();
+}
 
-export function handleFlashLoan(event: FlashLoan): void {}
+export function handleCreateMarket(event: CreateMarket): void {
+	let market = new Market(event.params.id);
+	market.lltv = event.params.marketParams.lltv;
+	market.lastTotalSupplyAssets = BigInt.zero();
+	market.lastTotalBorrowAssets = BigInt.zero();
+	market.lastTotalSupplyAssets = BigInt.zero();
+	market.lastTotalBorrowAssets = BigInt.zero();
 
-export function handleIncrementNonce(event: IncrementNonce): void {}
+	market.loanToken = getOrCreateAsset(event.params.marketParams.loanToken).id;
+	market.collateralToken = getOrCreateAsset(
+		event.params.marketParams.collateralToken,
+	).id;
 
-export function handleLiquidate(event: Liquidate): void {}
+	market.oracle = getOrCreateOracle(
+		event.params.marketParams.oracle,
+		event.block.timestamp,
+	).id;
 
-export function handleRepay(event: Repay): void {}
+	market.save();
+}
 
-export function handleSetAuthorization(event: SetAuthorization): void {}
+export function handleLiquidate(event: Liquidate): void {
+	const market = Market.load(event.params.id);
+	if (!market) {
+		log.critical("Market {} not found", [event.params.id.toString()]);
+		return;
+	}
+	// The number of borrow shares burn
+	const totalBorrowSharesReduced = event.params.repaidShares.plus(
+		event.params.badDebtShares,
+	);
 
-export function handleSetFee(event: SetFee): void {}
+	market.lastTotalBorrowShares = market.lastTotalBorrowShares.minus(
+		totalBorrowSharesReduced,
+	);
+	market.lastTotalBorrowAssets = market.lastTotalBorrowAssets.minus(
+		event.params.repaidAssets,
+	);
+	market.save();
 
-export function handleSetFeeRecipient(event: SetFeeRecipient): void {}
+	const position = Position.load(event.params.id.concat(event.params.borrower));
+	if (!position) {
+		// It cannot happen since position should have been created at the collateral deposit.
+		log.critical("Position {} not found", [
+			event.params.id.concat(event.params.borrower).toString(),
+		]);
+		return;
+	}
+	position.borrowShares = position.borrowShares.minus(totalBorrowSharesReduced);
+	position.collateral = position.collateral.minus(event.params.seizedAssets);
 
-export function handleSetOwner(event: SetOwner): void {}
+	// computeLtv(position);
+	position.save();
+}
 
-export function handleSupply(event: Supply): void {}
+export function handleRepay(event: Repay): void {
+	const market = Market.load(event.params.id);
+	if (!market) {
+		log.critical("Market {} not found", [event.params.id.toString()]);
+		return;
+	}
 
-export function handleSupplyCollateral(event: SupplyCollateral): void {}
+	market.lastTotalBorrowShares = market.lastTotalBorrowShares.minus(
+		event.params.shares,
+	);
+	market.lastTotalBorrowAssets = market.lastTotalBorrowAssets.minus(
+		event.params.assets,
+	);
+	market.save();
 
-export function handleWithdraw(event: Withdraw): void {}
+	const position = Position.load(event.params.id.concat(event.params.onBehalf));
+	if (!position) {
+		// It cannot happen since position should have been created at the collateral deposit.
+		log.critical("Position {} not found", [
+			event.params.id.concat(event.params.onBehalf).toString(),
+		]);
+		return;
+	}
+	position.borrowShares = position.borrowShares.minus(event.params.shares);
 
-export function handleWithdrawCollateral(event: WithdrawCollateral): void {}
+	// computeLtv(position);
+	position.save();
+}
+
+export function handleSupplyCollateral(event: SupplyCollateral): void {
+	const position = Position.load(event.params.id.concat(event.params.onBehalf));
+	if (!position) {
+		// It cannot happen since position should have been created at the collateral deposit.
+		log.critical("Position {} not found", [
+			event.params.id.concat(event.params.onBehalf).toString(),
+		]);
+		return;
+	}
+	position.collateral = position.collateral.plus(event.params.assets);
+	// computeLtv(position);
+	position.save();
+}
+
+export function handleWithdrawCollateral(event: WithdrawCollateral): void {
+	const position = Position.load(event.params.id.concat(event.params.onBehalf));
+	if (!position) {
+		// It cannot happen since position should have been created at the collateral deposit.
+		log.critical("Position {} not found", [
+			event.params.id.concat(event.params.onBehalf).toString(),
+		]);
+		return;
+	}
+
+	position.collateral = position.collateral.minus(event.params.assets);
+	computeLtv(position);
+	position.save();
+}
